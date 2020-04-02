cc7d26bdf55b58a4db2ca3cde118a09d6d000e84
Nodira J <raytsentr@gmail.com>
2020-04-02 01:01:58 -0400

    day3

---
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -0,0 +1,21 @@
+package com.automation.tests.day3;
+
+import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("Full_name"));
+
+
+    }
+}
